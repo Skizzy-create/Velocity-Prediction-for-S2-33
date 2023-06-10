ef8b89f1c4fe072c5753f0a5b27ae3d0319b8de8
Kartik Aslia <s7011068163@gmail.com>
2023-06-10 19:16:07 +0530

    Library version displayer

---
+++ Lib_Version.py
@@ -0,0 +1,20 @@
+import sys
+print("Python version : {}".format(sys.version))
+
+import pandas as pd
+print("Pandas version : {}".format(pd.__version__))
+
+import matplotlib
+print("Matplotlib version :  {}".format(matplotlib.__version__))
+
+import numpy as np
+print("Numpy version : {}".format(np.__version__))
+
+import scipy as sp
+print("Numpy version : {}".format(sp.__version__))
+
+import IPython
+print("IPython version: {}".format(IPython.__version__))
+
+import sklearn
+print("Sklearn version : {}".format(sklearn.__version__))
